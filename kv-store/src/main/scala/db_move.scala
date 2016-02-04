6755134fd7275529c111a84ed46662910ebd7c1c
Sean Pietz <spietz701@yahoo.com>
2016-02-04 01:52:41 -0500

    pure functional kv-store

--- kv-store/src/main/scala/db_move.scala
+++ kv-store/src/main/scala/db_move.scala
@@ -2,6 +2,22 @@ package sandbox
 
 import scala.reflect.runtime.universe._
 
+case class State[S, +A](run: S => (A, S)) {
+  def unit[S, A](a: A): State[S, A] =
+      State(s => (a, s))
+
+  def map[B](f: A => B): State[S, B] =
+    flatMap(a => unit(f(a)))
+
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+}
+
 object KVStore {
   abstract class Key(name: String) {
     type Value
@@ -9,45 +25,50 @@ object KVStore {
 
   def newKey[A : TypeTag](name: String) =
     new Key(name) {
-      val typ = typeOf[A]
       type Value = A
     }
+  
+  type KVAction[+A] = State[KVStore, A]
+
+  def get(key: Key): KVAction[Option[key.Value]] = 
+    State({ s: KVStore => s.get(key) })
+
+  def set(key: Key)(value: key.Value): KVAction[Unit] = 
+    State({ s: KVStore => s.set(key)(value) })
+
+  def modify(key: Key)(f: Option[key.Value] => key.Value): KVAction[Unit] =
+    get(key).map(f).map(v => set(key)(v))
 }
 
-class KVStore {
-  import KVStore.Key
-  import collection.mutable.Map
 
-  val store = Map.empty[Key, Any]
-  def get(key: Key): Option[key.Value] = store.get(key).asInstanceOf[Option[key.Value]]
-  def set(key: Key)(value: key.Value): Unit = store.update(key, value)
-
-  def setNew[A : TypeTag](keyName: String)(value: A) = {
-    val key = KVStore.newKey[A](keyName)
-    set(key)(value)
-    key
-  }
+import KVStore.Key
+case class KVStore(store: Map[KVStore.Key, Any] = Map.empty[KVStore.Key, Any] ) {
+  
+  type KVRes[A] = (A, KVStore)
+  
+  def get(key: Key): KVRes[Option[key.Value]] =
+    (store.get(key).asInstanceOf[Option[key.Value]], this)
+  
+  def set(key: Key)(value: key.Value): KVRes[Unit] = 
+    ((), KVStore(store + (key -> value)))
 }
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val dataStore = new KVStore
-
+    import KVStore.{get, set, newKey}
+    
     val foo = KVStore.newKey[Int]("foo")
     val bar = KVStore.newKey[String]("bar")
     
-    dataStore.set(foo)(123)
-    dataStore.set(bar)("hello")
-    dataStore.get(foo) // result: Option[Int] = Some(123)
+    val proc = for {
+      _ <- set(foo)(123)
+      _ <- set(bar)("Hello")
+      f <- get(foo)
+      b <- get(bar)
+    } yield (f.getOrElse(0) * 2).toString + " " + b.getOrElse("") + "!"
 
-    val baz = dataStore.setNew("baz")(true)
-
-    dataStore.get(baz) // result: Option[Boolean] = Some(true)
-    dataStore.set(baz)(false)
-
-    dataStore.set(baz)(8) // Fails compilation
-    dataStore.set(bar)(true) // Fails compilation
-    dataStore.set(foo)("test") // Fails compilation
+    val s = KVStore()
+    println(proc.run(s)._1)
   }
 }
